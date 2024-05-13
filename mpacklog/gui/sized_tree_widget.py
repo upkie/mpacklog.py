ca83f389fb88f4fff6d529068ad073921aa600ac
Stéphane Caron <stephane.caron@inria.fr>
2024-05-13 08:25:28 +0200

    Resize sized tree widget columns on the fly

--- mpacklog/gui/sized_tree_widget.py
+++ mpacklog/gui/sized_tree_widget.py
@@ -9,14 +9,41 @@
 # (https://github.com/mjbots/moteus, 49c698a63f0ded22528ad7539cc2e27e41cd486d)
 
 from qtpy import QtCore, QtWidgets
+from qtpy.QtWidgets import QHeaderView
 
 
 class SizedTreeWidget(QtWidgets.QTreeWidget):
-    def __init__(self, parent=None):
+    """Sized tree widget.
+
+    Attributes:
+        MIN_VALUE_WIDTH: Minimum width in pixels for the Value column.
+    """
+
+    COLUMN_PADDING: int = 10
+    MIN_VALUE_WIDTH: int = 100
+
+    def __init__(self, parent=None) -> None:
+        """Initialized tree widget.
+
+        Args:
+            parent: Parent Qt widget.
+        """
         QtWidgets.QTreeWidget.__init__(self, parent)
         self.setColumnCount(2)
         self.headerItem().setText(0, "Name")
         self.headerItem().setText(1, "Value")
 
+        self.header().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.header().setSectionResizeMode(0, QHeaderView.Interactive)
+        self.header().setSectionResizeMode(1, QHeaderView.Interactive)
+        self.header().setDefaultAlignment(QtCore.Qt.AlignLeft)
+
+    def resizeEvent(self, event):
+        """Resize first column (Name) to keep the Value column width fixed."""
+        width = self.width() - self.MIN_VALUE_WIDTH - self.COLUMN_PADDING
+        self.setColumnWidth(0, width)
+        super().resizeEvent(event)
+
     def sizeHint(self):
-        return QtCore.QSize(350, 500)
+        """Initial guess for widget dimensions."""
+        return QtCore.QSize(250, 500)
