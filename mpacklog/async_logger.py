8ecd226b9703d7c232d4def3966389df784c0d69
Stéphane Caron <stephane.caron@inria.fr>
2024-05-17 11:37:15 +0200

    Wait for logger to stop properly

--- mpacklog/async_logger.py
+++ mpacklog/async_logger.py
@@ -23,6 +23,7 @@ class AsyncLogger:
         Args:
             path: Path to the output log file.
         """
+        self.__writing = False
         self.__keep_going = True
         self.path = path
         self.queue = asyncio.Queue()
@@ -38,24 +39,27 @@ class AsyncLogger:
     async def stop(self):
         """Break the loop of the `write` coroutine."""
         self.__keep_going = False
+        while self.__writing:
+            await self.put({"exit": True})
+            await asyncio.sleep(0.01)
 
     async def write(self, flush: bool = False):
         """Continuously write messages from the logging queue to file."""
-        file = await aiofiles.open(self.path, "wb")
-        packer = msgpack.Packer(default=serialize, use_bin_type=True)
-        keep_going = not self.queue.empty() if flush else self.__keep_going
-        while keep_going:
-            message = await self.queue.get()
-            if message == {"exit": True}:
-                break
-            await file.write(packer.pack(message))
-            # Flushing has little effect when the Python process is configured
-            # more predictable although a little bit lower on average.
-            # on its own core (CPUID). When running on the default core, it
-            # tends to make the slack duration of the other coroutines
-            await file.flush()
+        assert not self.__writing
+        self.__writing = True
+        async with aiofiles.open(self.path, "wb") as file:
+            packer = msgpack.Packer(default=serialize, use_bin_type=True)
             keep_going = not self.queue.empty() if flush else self.__keep_going
-        await file.close()
+            while keep_going:
+                message = await self.queue.get()
+                if message == {"exit": True}:
+                    break
+                await file.write(packer.pack(message))
+                await file.flush()
+                keep_going = (
+                    not self.queue.empty() if flush else self.__keep_going
+                )
+        self.__writing = False
 
     async def flush(self):
         """Flush messages from the logging queue to file."""
